79fdbd3ae644bd3a87b377177219c8aea41b6649
UrbaZahid <urbazahid299@gmail.com>
2026-04-30 03:43:55 +0500

    update cors for vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const app = express();
 
 // ─── Middleware ───────────────────────────────────────────────
 app.use(cors({
-  origin: 'http://localhost:3000',
+   origin:
+   [
+    'http://localhost:3000',
+    'fyp-woad-chi.vercel.app'
+  ],
   credentials: true
 }));
 app.use(express.json());
